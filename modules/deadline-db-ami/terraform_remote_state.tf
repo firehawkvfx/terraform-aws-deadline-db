381d992039430e507d626463a654dfb4ed990539
Andrew Graham <andrew@firehawkvfx.com>
2021-03-07 17:23:27 +1030

    aquire region

--- modules/deadline-db-ami/terraform_remote_state.tf
+++ modules/deadline-db-ami/terraform_remote_state.tf
@@ -1,20 +1,17 @@
 # A module to read the desired instance profile from terraform remote state.
-
 variable "bucket_extension_vault" {
     description = "The bucket extension where the terraform remote state resides"
     type = string
 }
-
 variable "resourcetier_vault" {
     description = "The resourcetier the desired vault vpc resides in"
     type = string
 }
-
 variable "vpcname_vault" {
     description = "A namespace component defining the location of the terraform remote state"
     type = string
 }
-
+data "aws_region" "current" {}
 data "terraform_remote_state" "provisioner_profile" { # read the arn with data.terraform_remote_state.provisioner_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.provisioner_profile.outputs.instance_profile_name
   backend = "s3"
   config = {
@@ -23,7 +20,6 @@ data "terraform_remote_state" "provisioner_profile" { # read the arn with data.t
     region = data.aws_region.current.name
   }
 }
-
 output "instance_profile_name" {
     value = data.terraform_remote_state.provisioner_profile.outputs.instance_profile_name
 }
