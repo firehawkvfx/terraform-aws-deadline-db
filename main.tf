f116de573407d666a7fc8d4c30de67d733a55db0
Andrew Graham <andrew@firehawkvfx.com>
2021-03-22 14:23:38 +1030

    update names

--- main.tf
+++ main.tf
@@ -84,14 +84,12 @@ data "terraform_remote_state" "bastion_security_group" { # read the arn with dat
 }
 
 locals {
-  mount_path           = var.resourcetier
-  vpc_id               = data.aws_vpc.primary.id
-  vpc_cidr             = data.aws_vpc.primary.cidr_block
-  aws_internet_gateway = data.aws_internet_gateway.gw.id
-
+  mount_path                 = var.resourcetier
+  vpc_id                     = data.aws_vpc.primary.id
+  vpc_cidr                   = data.aws_vpc.primary.cidr_block
+  aws_internet_gateway       = data.aws_internet_gateway.gw.id
   vpn_cidr                   = var.vpn_cidr
   onsite_private_subnet_cidr = var.onsite_private_subnet_cidr
-
   private_subnet_ids         = tolist(data.aws_subnet_ids.private.ids)
   private_subnet_cidr_blocks = [for s in data.aws_subnet.private : s.cidr_block]
   private_domain             = lookup(data.vault_generic_secret.private_domain.data, "value")
@@ -101,22 +99,19 @@ locals {
   # public_domain_name         = "none"
 }
 module "deadline_db_vault_client" {
-  source             = "./modules/deadline-db-vault-client"
-  name               = "deadlinedbvaultclient_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
-  deadline_db_ami_id = var.deadline_db_ami_id
-
-  consul_cluster_name    = var.consul_cluster_name
-  consul_cluster_tag_key = var.consul_cluster_tag_key
-  aws_internal_domain    = var.aws_internal_domain
-  vpc_id                 = local.vpc_id
-  vpc_cidr               = local.vpc_cidr
-
+  source                      = "./modules/deadline-db-vault-client"
+  name                        = "${lookup(local.common_tags, "vpcname", "default")}_deadlinedbvaultclient_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
+  deadline_db_ami_id          = var.deadline_db_ami_id
+  consul_cluster_name         = var.consul_cluster_name
+  consul_cluster_tag_key      = var.consul_cluster_tag_key
+  aws_internal_domain         = var.aws_internal_domain
+  vpc_id                      = local.vpc_id
+  vpc_cidr                    = local.vpc_cidr
   bucket_extension_vault      = var.bucket_extension_vault
   private_subnet_ids          = local.private_subnet_ids
   permitted_cidr_list         = ["${local.onsite_public_ip}/32", var.remote_cloud_public_ip_cidr, var.remote_cloud_private_ip_cidr, local.onsite_private_subnet_cidr, local.vpn_cidr]
   permitted_cidr_list_private = [var.remote_cloud_private_ip_cidr, local.onsite_private_subnet_cidr, local.vpn_cidr, local.vpc_cidr]
   security_group_ids          = [data.terraform_remote_state.bastion_security_group.outputs.security_group_id]
-
-  aws_key_name = var.aws_key_name
-  common_tags  = local.common_tags
+  aws_key_name                = var.aws_key_name
+  common_tags                 = local.common_tags
 }
