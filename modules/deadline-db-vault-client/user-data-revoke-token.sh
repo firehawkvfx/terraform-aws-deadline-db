0e2f8793f70951d3227283c700c1a15703c045c5
Andrew Graham <andrew@firehawkvfx.com>
2021-04-04 17:53:22 +0930

    append to userdata

--- modules/deadline-db-vault-client/user-data-revoke-token.sh
+++ modules/deadline-db-vault-client/user-data-revoke-token.sh
@@ -6,7 +6,7 @@
 set -e
 
 # Send the log output from this script to user-data.log, syslog, and the console. From: https://alestic.com/2010/12/ec2-user-data-output/
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+exec > >(tee -a /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # Log the given message. All logs are written to stderr with a timestamp.
 function log {
